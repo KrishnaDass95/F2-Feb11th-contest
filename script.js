70fd1819d5bf697c5980b220bf2edb05b3e43f9b
KrishnaDass95 <krishdass95@gmail.com>
2023-02-11 17:13:13 +0530

    Finish the first three functions

--- script.js
+++ script.js
@@ -7,19 +7,44 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  arr.map(function(student){
+    if(student.marks > 50){
+      console.log(student);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(function (student){
+    if(student.marks > 50){
+      console.log(student);
+    }
+  })
 }
 
 function addData() {
-  //Write your code here, just console.log
+  arr.push(
+    {id:4,name:"susan",age:"20",marks:45}
+  );
+  console.log(arr[arr.length - 1]);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  // adding the indices of the students to be removed
+  var toBeRemovedStudentIndex = [];
+  arr.forEach(function(student, index){
+    if(student.marks < 50){
+      toBeRemovedStudentIndex.push(index);
+    }
+  })
+  // lets reverse the array index from above so that splice function does not mess with index ordering
+  toBeRemovedStudentIndex.reverse().forEach(function(index){
+    arr.splice(index, 1);
+  })
+  // logging the passed students
+  arr.forEach(function(student){
+    console.log(student);
+  })
 }
 
 function concatenateArray() {
